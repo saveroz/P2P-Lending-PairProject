93c97cb7521e218a68d3a13e68537beaaa42f504
saveroz <muhammadsavero@gmail.com>
2019-08-02 07:41:04 +0700

    ready to deploy

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,37 +4,43 @@ const {generatePass, checkPassword} = require('../helper/encryptpass')
 class LoginController{
 
     static AuthLogin(req,res){
-        let data = req.body
+    let data = req.body
     // res.send(data)
     User.findOne({
         where : {
             email : data.email
         }
     }).then((User) =>{
-        let pass = data.password
-        if (checkPassword(pass,User.password)){
-
-            req.session.currentUser = {
-                'email' : User.email,
-                'password' : User.password,
-                'id' : User.id
-            }
-
-            if (User.role === 'user'){
-
-                // console.log(req.sessionCurrentUser)
-                res.redirect('/login/user')
+        if (User){
+            let pass = data.password
+            if (checkPassword(pass,User.password)){
+    
+                req.session.currentUser = {
+                    'email' : User.email,
+                    'password' : User.password,
+                    'id' : User.id
+                }
+    
+                if (User.role === 'user'){
+    
+                    // console.log(req.sessionCurrentUser)
+                    res.redirect('/login/user')
+                    
+                }
+                else{
+                    res.redirect('/login/admin')
+                }
                 
             }
             else{
-                res.redirect('/login/admin')
+                // res.redirect('/Home')
+                res.send('login gagal')
             }
-            
         }
         else{
-            // res.redirect('/Home')
-            res.send('login gagal')
+            throw new Error('Email not Found')
         }
+       
 
     }).catch(err=>{
         res.send(err.message)
